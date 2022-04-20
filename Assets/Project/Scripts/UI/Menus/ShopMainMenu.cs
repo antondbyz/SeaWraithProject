234d4225145ab3e09e3d7c3de423690b31804290
EighterDev <EighterInc@gmail.com>
2022-04-20 15:21:14 +0300

    Working on redesigning shop ui

--- Assets/Project/Scripts/UI/Menus/ShopMainMenu.cs
+++ Assets/Project/Scripts/UI/Menus/ShopMainMenu.cs
@@ -4,33 +4,15 @@ using UnityEngine.UI;
 
 public class ShopMainMenu : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _statsText;
+    [SerializeField] private TMP_Text _currentSubmarineStatsText;
     [SerializeField] private Image _currentSubmarinePreview;
     [SerializeField] private GameObject _maxLevelLabel;
 
     private void OnEnable()
     {
-        UpdateUI();
-    }
-
-    private void UpdateUI()
-    {
         _currentSubmarinePreview.sprite = SubmarinePaintsManager.CurrentPaintObject.Paint;
-        _statsText.text = $"Armor: {SubmarineStatsManager.CurrentStatsObject.Armor}" +
-                            $"\nMobility: {SubmarineStatsManager.CurrentStatsObject.Mobility}";
+        _currentSubmarineStatsText.text = $"Armor: {SubmarineStatsManager.CurrentStatsObject.Armor}" +
+            $"\nMobility: {SubmarineStatsManager.CurrentStatsObject.Mobility}"; 
         _maxLevelLabel.SetActive(SubmarineStatsManager.NextStatsObject == null);
-        string nextArmorText = "";
-        string nextMobilityText = "";
-        if (isThereNextSubmarineItem) 
-        {
-            nextArmorText = $"(+{SubmarineStatsManager.NextStatsObject.Armor - currentArmor})";
-            nextMobilityText = $"(+{SubmarineStatsManager.NextStatsObject.Mobility - currentMobility})";
-            bool isEnoughCrystals = PlayerManager.CrystalsAmount >= SubmarineStatsManager.NextStatsObject.Price;
-            string upgradeTextColor = isEnoughCrystals ? "green" : "red";
-            string priceText = $"{SubmarineStatsManager.NextStatsObject.Price}";
-            _upgradeButtonText.text = $"Upgrade <color={upgradeTextColor}>({priceText})</color><sprite name=Crystal>";
-        }
-        _statsText.text = $"Armor: {currentArmor} <color=green>{nextArmorText}</color>" +
-                            $"\nMobility: {currentMobility} <color=green>{nextMobilityText}</color>";
     }
 }
--- Assets/Project/Scripts/UI/Menus/UpgradeMenu.cs
+++ Assets/Project/Scripts/UI/Menus/UpgradeMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class UpgradeMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text _upgradeInfo;
+    [SerializeField] private TMP_Text _priceText;
     [SerializeField] private GameObject _buyButton;
     [SerializeField] private GameObject _notEnoughCrystalsLabel;
 
@@ -15,8 +16,29 @@ public class UpgradeMenu : MonoBehaviour
 
     private void OnEnable()
     {
+        UpdateUI();
+        SubmarineStatsManager.CurrentStatsObjectChanged += UpdateUI;
+        PlayerManager.CrystalsChanged += UpdateUI;
+    }
+
+    private void OnDisable()
+    {
+        SubmarineStatsManager.CurrentStatsObjectChanged -= UpdateUI;
+        PlayerManager.CrystalsChanged -= UpdateUI;
+    }
+
+    private void UpdateUI()
+    {
         bool isEnoughCrystals = PlayerManager.CrystalsAmount >= SubmarineStatsManager.NextStatsObject.Price;
         _buyButton.SetActive(isEnoughCrystals);
         _notEnoughCrystalsLabel.SetActive(!isEnoughCrystals);
+        _priceText.text = $"{SubmarineStatsManager.NextStatsObject.Price}<sprite name=Crystal>";
+        _priceText.color = isEnoughCrystals ? Color.green : Color.red;
+        int currentArmor = SubmarineStatsManager.CurrentStatsObject.Armor;
+        int currentMobility = SubmarineStatsManager.CurrentStatsObject.Mobility;
+        int armorProfit = SubmarineStatsManager.NextStatsObject.Armor - currentArmor;
+        int mobilityProfit = SubmarineStatsManager.NextStatsObject.Mobility - currentMobility;
+        _upgradeInfo.text = $"Armor: {currentArmor} <color=green>(+{armorProfit})</color>" +
+                            $"\nMobility: {currentMobility} <color=green>(+{mobilityProfit})</color>";
     }
 }
